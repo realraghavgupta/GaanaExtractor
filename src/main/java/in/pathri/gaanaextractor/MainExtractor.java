c4ce85e911baec4e08770ac45639942c37c05eec
pathrik <pathrikumark@gmail.com>
2015-04-12 23:28:14 +0530

    Initial Commit

---
+++ src/main/java/in/pathri/gaanaextractor/MainExtractor.java
@@ -0,0 +1,14 @@
+package in.pathri.gaanaextractor;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MainExtractor {
+	static final Logger logger = LogManager.getLogger(MainExtractor.class.getName());
+
+	public static void main(String[] args) {
+		logger.entry();
+		logger.exit();
+	}
+
+}
